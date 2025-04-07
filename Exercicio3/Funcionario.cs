235b20ec98a54001208b475a99393ded33b1b57b
alexr <alexr@DESKTOP-I9SIN13>
2025-04-07 00:40:32 -0300

    Exercicio 3 "meio feito"

---
+++ Exercicio3/Funcionario.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    internal class Funcionario
+    {
+        // Atributos privados
+        private string? _nome;
+        private string? _cpf;
+        private DateTime _dataAdmissao;
+        private double _salarioBase;
+
+        // Propriedades
+        public string? Nome
+        {
+            get { return _nome; }
+
+            set 
+            { 
+                _nome = value; 
+            }
+        }
+
+        public string? CPF
+        {
+            get { return _cpf; }
+
+            set 
+            { 
+                _cpf = value; 
+            }
+        }
+
+        public DateTime DataAdmissao
+        {
+            get { return _dataAdmissao; }
+
+            set 
+            { 
+                _dataAdmissao = value; 
+            }
+        }
+
+        public double SalarioBase
+        {
+            get { return _salarioBase; }
+
+            set 
+            { 
+                _salarioBase = value; 
+            }
+        }
+        
+        // Construtor
+        public Funcionario()
+        {
+            // Devo sempre inicializar?
+        }
+
+        // Métodos virtuais
+        public virtual void CalcularSalario()
+        {
+            Console.WriteLine(SalarioBase);
+        }
+        public virtual void ExibirDados()
+        {
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("CPF: " + CPF);
+            Console.WriteLine("Data de admissão: " + DataAdmissao.ToShortDateString());
+            Console.WriteLine("Salário: " + SalarioBase);
+        }
+
+
+    }
+}
--- Exercicio3/README.md
+++ Exercicio3/README.md
@@ -1,1 +1,22 @@
+Exercício 3: Sistema de RH (Herança)
+Objetivo
+Desenvolver um sistema de RH explorando hierarquia de classes e herança.
 
+Tarefas
+Crie uma classe base Funcionario com:
+
+Propriedades comuns (Nome, CPF, DataAdmissao, SalarioBase)
+Método virtual CalcularSalario()
+Método virtual ExibirDados()
+Crie as seguintes classes derivadas:
+
+Desenvolvedor (com especialidade e nível)
+Gerente (com equipe e bônus por resultados)
+Atendente (com meta de atendimentos e comissão)
+Sobrescreva os métodos adequadamente em cada classe.
+
+Demonstre o uso de base para chamar a implementação da classe base e estender o comportamento.
+
+Cenário de teste:
+
+Crie diferentes tipos de funcionários e processe a folha de pagamento mostrando o polimorfismo em ação.
